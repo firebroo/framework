30377dbd19328b24f1cc6917fe9c483e0f51c0be
firebroo <1121927992@qq.com>
2015-08-06 15:58:21 +0800

    update

---
+++ boot.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/8/6
+ * Time: 15:32
+ */
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', dirname(__FILE__));
+define('CONTROLLER_PATH', ROOT_PATH . DS . 'controller');
+define('DB_PATH', ROOT_PATH . DS . 'db');
+define('DAO_PATH', ROOT_PATH . DS . 'dao');
+define('VIEW_PATH', ROOT_PATH . DS . 'view');
+define('SERVICE_PATH', ROOT_PATH . DS . 'service');
+define('HELPER_PATH', ROOT_PATH . DS . 'helper');
+define('IOC_PATH', ROOT_PATH . DS . 'ioc');
+$controller = $_GET['controller'] ? $_GET['controller'] : "";
+if (file_exists(CONTROLLER_PATH . DS . $controller . "Controller.php")) {
+    include CONTROLLER_PATH . DS . $controller . "Controller.php";
+    include VIEW_PATH . DS . $controller . "View.php";
+    include SERVICE_PATH . DS . $controller . "Service.php";
+    include DAO_PATH . DS . $controller . "Dao.php";
+    include DB_PATH . "/DbMysqlImpl.php";
+    include HELPER_PATH . "/actionHelper.php";
+    include IOC_PATH . "/Ioc.php";
+} else {
+    exit("Controller not exist");
+}
+
+
+
+

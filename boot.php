63a1a78ea6ee2d656bdb369d2322c0064ac9979e
firebroo <1121927992@qq.com>
2015-08-06 16:02:50 +0800

    update

--- boot.php
+++ boot.php
@@ -14,18 +14,18 @@ define('VIEW_PATH', ROOT_PATH . DS . 'view');
 define('SERVICE_PATH', ROOT_PATH . DS . 'service');
 define('HELPER_PATH', ROOT_PATH . DS . 'helper');
 define('IOC_PATH', ROOT_PATH . DS . 'ioc');
+
 $controller = $_GET['controller'] ? $_GET['controller'] : "";
-if (file_exists(CONTROLLER_PATH . DS . $controller . "Controller.php")) {
-    include CONTROLLER_PATH . DS . $controller . "Controller.php";
-    include VIEW_PATH . DS . $controller . "View.php";
-    include SERVICE_PATH . DS . $controller . "Service.php";
-    include DAO_PATH . DS . $controller . "Dao.php";
-    include DB_PATH . "/DbMysqlImpl.php";
-    include HELPER_PATH . "/actionHelper.php";
-    include IOC_PATH . "/Ioc.php";
-} else {
+if (!file_exists(CONTROLLER_PATH . DS . $controller . "Controller.php")) {
     exit("Controller not exist");
 }
+include CONTROLLER_PATH . DS . $controller . "Controller.php";
+include VIEW_PATH . DS . $controller . "View.php";
+include SERVICE_PATH . DS . $controller . "Service.php";
+include DAO_PATH . DS . $controller . "Dao.php";
+include DB_PATH . "/DbMysqlImpl.php";
+include HELPER_PATH . "/actionHelper.php";
+include IOC_PATH . "/Ioc.php";
 
 
 

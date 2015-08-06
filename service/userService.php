2e75c1e9aede21c6b0904a5167b5975f8aa08cd2
firebroo <1121927992@qq.com>
2015-08-06 11:25:02 +0800

    update

--- service/userService.php
+++ service/userService.php
@@ -1,20 +1,24 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2015/8/6
  * Time: 10:22
  */
-
-class userService {
+class userService
+{
     private $userDao;
-    public function __construct($dao) {
+
+    public function __construct($dao)
+    {
         $this->userDao = $dao;
     }
 
-    public function saveUser($request) {
+    public function saveUser($request)
+    {
         $id = $request['id'];
         $name = $request['name'];
-        return $this->userDao->userInsert($id, $name)?"save Customer information successful":"error";
+        return $this->userDao->userInsert($id, $name) ? "save Customer information successful" : "error";
     }
 }
